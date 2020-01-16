74dad32637a26ac6faa60c91986214aac36fa95f
FreezerNick <nl@f2p-entertainment.de>
2020-01-16 08:23:46 +0100

    Dependency

--- Source/FreeXML/FreeXML.Build.cs
+++ Source/FreeXML/FreeXML.Build.cs
@@ -26,7 +26,7 @@ public class FreeXML : ModuleRules
 			new string[]
 			{
 				"Core",
-				// ... add other public dependencies that you statically link with here ...
+				"XmlParser",
 			}
 			);
 			
